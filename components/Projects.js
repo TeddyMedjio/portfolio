0f55aa6ae1d32823e9e2e438e96b3bbdfcdf87e6
Teddy Medjio <nganmedjiojeanclaude@gmail.com>
2022-12-22 18:53:43 +0100

    update class

--- components/Projects.js
+++ components/Projects.js
@@ -4,8 +4,8 @@ import Slideprojet from "./Slideprojet";
 const Projects = () => {
   return (
     <div className=" bg-[#012857] h-[926px] my-28">
-      <div class="relative z-0 h-[926px]">
-        <div class=" gradient-mask absolute inset-0 w-full overflow-x-hidden">
+      <div className="relative z-0 h-[926px]">
+        <div className=" gradient-mask absolute inset-0 w-full overflow-x-hidden">
           <div className="centrer h-[500px] w-[1180px] whitespace-nowrap">
             <h2 className="text-5xl text-center font-anton text-white mb-20">
               Derniers projets
@@ -13,43 +13,43 @@ const Projects = () => {
             <Slideprojet />
           </div>
         </div>
-        <div class="z-0 grid grid-cols-8 grid-rows-12 border border-white/20 pt-16 ">
-          <div class="aspect-square border border-white/20"></div>
-          <div class="aspect-square border border-white/20"></div>
-          <div class="aspect-square border border-white/20"></div>
-          <div class="aspect-square border border-white/20"></div>
-          <div class="aspect-square border border-white/20"></div>
-          <div class="aspect-square border border-white/20"></div>
-          <div class="aspect-square border border-white/20"></div>
-          <div class="aspect-square border border-white/20"></div>
-          <div class="aspect-square border border-white/20"></div>
-          <div class="aspect-square border border-white/20 bg-white/5"></div>
-          <div class="aspect-square border border-white/20"></div>
-          <div class="aspect-square border border-white/20"></div>
-          <div class="aspect-square border border-white/20"></div>
-          <div class="aspect-square border border-white/20"></div>
-          <div class="aspect-square border border-white/20"></div>
-          <div class="aspect-square border border-white/20"></div>
-          <div class="aspect-square border border-white/20"></div>
-          <div class="aspect-square border border-white/20"></div>
-          <div class="aspect-square border border-white/20"></div>
-          <div class="aspect-square border border-white/20"></div>
-          <div class="aspect-square border border-white/20 bg-white/5"></div>
-          <div class="aspect-square border border-white/20"></div>
-          <div class="aspect-square border border-white/20"></div>
-          <div class="aspect-square border border-white/20"></div>
-          <div class="aspect-square border border-white/20"></div>
-          <div class="aspect-square border border-white/20"></div>
-          <div class="aspect-square border border-white/20"></div>
-          <div class="aspect-square border border-white/20"></div>
-          <div class="aspect-square border border-white/20 bg-white/5"></div>
-          <div class="aspect-square border border-white/20"></div>
-          <div class="aspect-square border border-white/20"></div>
-          <div class="aspect-square border border-white/20 bg-white/5"></div>
-          <div class="aspect-square border border-white/20"></div>
-          <div class="aspect-square border border-white/20"></div>
-          <div class="aspect-square border border-white/20"></div>
-          <div class="aspect-square border border-white/20"></div>
+        <div className="z-0 grid grid-cols-8 grid-rows-12 border border-white/20 pt-16 ">
+          <div className="aspect-square border border-white/20"></div>
+          <div className="aspect-square border border-white/20"></div>
+          <div className="aspect-square border border-white/20"></div>
+          <div className="aspect-square border border-white/20"></div>
+          <div className="aspect-square border border-white/20"></div>
+          <div className="aspect-square border border-white/20"></div>
+          <div className="aspect-square border border-white/20"></div>
+          <div className="aspect-square border border-white/20"></div>
+          <div className="aspect-square border border-white/20"></div>
+          <div className="aspect-square border border-white/20 bg-white/5"></div>
+          <div className="aspect-square border border-white/20"></div>
+          <div className="aspect-square border border-white/20"></div>
+          <div className="aspect-square border border-white/20"></div>
+          <div className="aspect-square border border-white/20"></div>
+          <div className="aspect-square border border-white/20"></div>
+          <div className="aspect-square border border-white/20"></div>
+          <div className="aspect-square border border-white/20"></div>
+          <div className="aspect-square border border-white/20"></div>
+          <div className="aspect-square border border-white/20"></div>
+          <div className="aspect-square border border-white/20"></div>
+          <div className="aspect-square border border-white/20 bg-white/5"></div>
+          <div className="aspect-square border border-white/20"></div>
+          <div className="aspect-square border border-white/20"></div>
+          <div className="aspect-square border border-white/20"></div>
+          <div className="aspect-square border border-white/20"></div>
+          <div className="aspect-square border border-white/20"></div>
+          <div className="aspect-square border border-white/20"></div>
+          <div className="aspect-square border border-white/20"></div>
+          <div className="aspect-square border border-white/20 bg-white/5"></div>
+          <div className="aspect-square border border-white/20"></div>
+          <div className="aspect-square border border-white/20"></div>
+          <div className="aspect-square border border-white/20 bg-white/5"></div>
+          <div className="aspect-square border border-white/20"></div>
+          <div className="aspect-square border border-white/20"></div>
+          <div className="aspect-square border border-white/20"></div>
+          <div className="aspect-square border border-white/20"></div>
         </div>
       </div>
     </div>
